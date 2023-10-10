38f6ab2e2ba89316ff523ef8900aeb570b5d5d24
chudo yudo <chudoyudo@hotmail.com>
2023-10-10 19:19:29 +0000

    updated

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ app = Flask(__name__)
 model = load_model('./model/content/model')
 scaler = joblib.load('./scaler.pkl')
 
-# prediction function
-# def ValuePredictor(to_predict_list):
-#     to_predict = np.array(to_predict_list).reshape(1, 12)
-#     loaded_model = pickle.load(open("model.pkl", "rb"))
-#     result = loaded_model.predict(to_predict)
-#     return result[0]
+
 
 @app.route('/result', methods=['POST'])
 def predict():
@@ -31,18 +26,6 @@ def predict():
 def home():
     return render_template('index.html')
 
-# @app.route('/result', methods = ['POST'])
-# def result():
-#     if request.method == 'POST':
-#         to_predict_list = request.form.to_dict()
-#         to_predict_list = list(to_predict_list.values())
-#         to_predict_list = list(map(int, to_predict_list))
-#         result = ValuePredictor(to_predict_list)        
-#         if int(result)== 1:
-#             prediction ='Income more than 50K'
-#         else:
-#             prediction ='Income less that 50K'           
-#         return render_template("result.html", prediction = prediction)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
