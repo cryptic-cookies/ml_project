6efc21f5f937d3d622ac527de189345e8a34b9fc
cryptic-cookies <92694887+cryptic-cookies@users.noreply.github.com>
2023-10-10 11:55:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
@@ -9,10 +9,18 @@ app = Flask(__name__)
 model = load_model('./model/content/model2')
 scaler = joblib.load('./scaler.pkl')
 
-@app.route('/predict', methods=['POST'])
+# prediction function
+# def ValuePredictor(to_predict_list):
+#     to_predict = np.array(to_predict_list).reshape(1, 12)
+#     loaded_model = pickle.load(open("model.pkl", "rb"))
+#     result = loaded_model.predict(to_predict)
+#     return result[0]
+
+@app.route('/result', methods=['POST'])
 def predict():
     try:
-        input_data = np.array([request.json['data']])
+        to_predict_list = request.form.to_dict()
+        input_data = np.array(to_predict_list) #([request.json['data']])
         scaled_data = scaler.transform(input_data)
         prediction = model.predict(scaled_data)
         return jsonify(prediction.tolist())
@@ -22,6 +30,19 @@ def predict():
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+# @app.route('/result', methods = ['POST'])
+# def result():
+#     if request.method == 'POST':
+#         to_predict_list = request.form.to_dict()
+#         to_predict_list = list(to_predict_list.values())
+#         to_predict_list = list(map(int, to_predict_list))
+#         result = ValuePredictor(to_predict_list)        
+#         if int(result)== 1:
+#             prediction ='Income more than 50K'
+#         else:
+#             prediction ='Income less that 50K'           
+#         return render_template("result.html", prediction = prediction)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
